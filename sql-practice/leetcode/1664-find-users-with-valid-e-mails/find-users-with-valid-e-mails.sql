d016e108dcee54f68f0c01930c24d237a111c17c
Athul K R <athulkr34@gmail.com>
2026-08-16 21:12:37 +0530

    Time: 947 ms (24.28%) | Memory: 0B (100.00%) - LeetSync

--- sql-practice/leetcode/1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ sql-practice/leetcode/1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,2 +1,9 @@
-SELECT * FROM Users
-WHERE REGEXP_LIKE(mail, '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$', 'c');
+SELECT 
+    user_id, 
+    name, 
+    mail
+FROM 
+    users
+WHERE 
+    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' 
+    AND mail LIKE BINARY '%@leetcode.com';
